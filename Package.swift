aa1b0794ab26b1f9d5793c076b21809f686d2c51
Gerry Weißbach <gerry.w@gammaproduction.de>
2019-09-29 13:10:36 +0200

    Rename Package to fit GitHub url

--- Package.swift
+++ Package.swift
@@ -4,11 +4,11 @@
 import PackageDescription
 
 let package = Package(
-    name: "FTMS Bluetooth",
+    name: "FTMS-Bluetooth",
     products: [
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
-            name: "FTMS Bluetooth",
+            name: "FTMS-Bluetooth",
             targets: ["FTMS Bluetooth"]),
     ],
     dependencies: [
